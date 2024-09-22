798b1f5c5a583ecfef2bc4052ae0d12d0544eecc
pankajkm884 <pankajkm884@gmail.com>
2024-09-22 11:42:42 +0530

    Controller class name is updated

--- src/main/java/com/info/spring_web_test/controller/Controller.java
+++ src/main/java/com/info/spring_web_test/controller/Controller.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/users")
-public class controller {
+public class Controller {
 
     List<User> users = new ArrayList<>();
     @Value("${database.username}")
